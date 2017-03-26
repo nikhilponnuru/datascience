0a67e6aef439c52c5f95947eca187a4977157e9d
nikhil ponnuru <nikkifoundation.nikhil@gmail.com>
2017-03-27 01:21:46 +0530

    Update data-analyzing.py

--- data-analyzing.py
+++ data-analyzing.py
@@ -11,7 +11,7 @@ from sklearn import metrics
 
 #reading the data from csv file to data frame of pandas
 
-df=pd.read_csv("/home/nikhilponnuru/Desktop/project-datascience/train_u6lujuX_CVtuZ9i.csv")
+df=pd.read_csv("/home/nikhilponnuru/Desktop/project-datascience/trainingdata.csv")
 
 print(df.head(10))  #used to display top 10 rows
 print (df.describe())
@@ -50,10 +50,11 @@ ax2.set_ylabel('Probability of getting loan')
 ax2.set_title("Probability of getting loan by credit history")
 
 
-
+#displying 2 graphs into one
 temp3 = pd.crosstab(df['Credit_History'], df['Loan_Status'])
 temp3.plot(kind='bar', stacked=True, color=['red','blue'], grid=False)
 
+#Check missing values in the dataset
 
 df['LoanAmount'].fillna(df['LoanAmount'].mean(), inplace=True)
 
@@ -111,7 +112,9 @@ def classification_model(model, data, predictors, outcome):
    
     model.fit(data[predictors], data[outcome])
 
+      #regression analysis
 
+      
 outcome_var = df['Loan_Status']
 model = LogisticRegression()
 predictor_var = df['Credit_History']
